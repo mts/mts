062a7282c77c40ffc4592ddc224e376ec5b60ee7
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-13 20:11:51 +0100

    use port 3002 instead of 3001

--- packages/server/src/index.js
+++ packages/server/src/index.js
@@ -11,7 +11,7 @@ server.get('/', (req, res) => {
   res.send('MTS GitHub IO!')
 })
 
-const SERVER_PORT = process.env.SERVER_PORT || 3001
+const SERVER_PORT = process.env.SERVER_PORT || 3002
 
 server.listen(SERVER_PORT, () => {
   console.log(`🚀 Server listening on port ${SERVER_PORT}`)
