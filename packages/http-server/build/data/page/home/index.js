6301b854cfc865dcf83c3d3cbabb81f89242ba66
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-13 21:28:03 +0100

    rename server to http server

---
+++ packages/http-server/build/data/page/home/index.js
@@ -0,0 +1,10 @@
+const {
+  infoData
+} = require('./info');
+
+module.exports.homePageData = {
+  info: {
+    id: infoData.id,
+    name: infoData.name
+  }
+};
---
+++ packages/http-server/build/data/page/index.js
@@ -0,0 +1,9 @@
+const {
+  homePageData
+} = require('./home');
+
+const {
+  notFoundPageData
+} = require('./notFound');
+
+module.exports.pageData = [homePageData, notFoundPageData];
