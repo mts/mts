ce654e7748523fb9a89d0e376d5081d5da49d69d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-14 14:53:12 +0100

    render page

--- packages/http-server/src/index.js
+++ packages/http-server/src/index.js
@@ -1,17 +1,63 @@
 /* eslint no-console: 0 */
-require('dotenv').config()
-const cors = require('cors')
-const express = require('express')
+import dotenv from 'dotenv'
+import cors from 'cors'
+import express from 'express'
+import React from 'react'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { renderToString } from 'react-dom/server'
+// import { RegularApp } from '../../client/src'
+
+function renderFullPage(html, finalState) {
+  return `
+    <!doctype html>
+    <html>
+      <head>
+        <title>Redux Universal Example</title>
+      </head>
+      <body>
+        <div id="app">${html}</div>
+        <script>
+          window.__PRELOADED_STATE__ = ${JSON.stringify(finalState).replace(/</g, '\\u003c')}
+        </script>
+        <script src="/static/bundle.js"></script>
+      </body>
+    </html>
+    `
+}
+
+function someReducer(state = [], action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+function handleRender(req, res) {
+  const store = createStore(someReducer, { someState: 'some-state' })
+
+  const html = renderToString(
+    <Provider store={store}>
+      <div>test</div>
+    </Provider>,
+  )
+
+  const finalState = store.getState()
+
+  res.send(renderFullPage(html, finalState))
+}
+
+dotenv.config()
 
 const server = express()
 
 server.use(cors())
 
-server.get('/', (req, res) => {
-  res.send('Hello MTS HTTP Server!')
-})
+server.use('/static', express.static('static'))
 
-const PORT = process.env.PORT || 3002
+server.use(handleRender)
+
+const PORT = process.env.PORT || 3001
 
 server.listen(PORT, () => {
   console.log(`🚀 MTS HTTP Server listening on port ${PORT}`)
