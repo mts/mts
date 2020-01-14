9acaa9e68553e5daac9c534ed161d09c043c4c1c
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-14 19:39:03 +0100

    require @babel/register on client

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,44 +1,12 @@
-/* eslint no-unused-vars : 0 */
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { withRouter } from 'react-router'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { homePageRoute, notFoundPageRoute } from './route'
-import { addDivWithIdToBody, checkAccessibilityIssues } from './library/startup'
-import { ErrorBoundary } from './pattern/atom/error/ErrorBoundary'
+/* eslint global-require: 0 */
 
-const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact={homePageRoute.exact} path={homePageRoute.path} component={homePageRoute.component} />
-      <Route component={withRouter(notFoundPageRoute.component)} />
-    </Switch>
-  </BrowserRouter>
-)
+require('@babel/register')({
+  extensions: ['.js', '.jsx'],
+  ignore: [],
+})
 
-addDivWithIdToBody('app')
-checkAccessibilityIssues(React, ReactDOM, 1000)
-
-export function StrictApp() {
-  return (
-    <React.StrictMode>
-      <ErrorBoundary>
-        <App />
-      </ErrorBoundary>
-    </React.StrictMode>
-  )
-}
-
-export function RegularApp() {
-  return (
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  )
-}
-
-ReactDOM.render(<RegularApp />, document.getElementById('app'))
-
-if (module.hot) {
-  module.hot.accept()
+if (typeof window === 'undefined') {
+  require('./serverApp')
+} else {
+  require('./clientApp')
 }
