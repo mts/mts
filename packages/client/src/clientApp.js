0c750e6d0e2c03655a2648f1a22e79551821083c
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-14 19:40:20 +0100

    add client app

---
+++ packages/client/src/clientApp.js
@@ -0,0 +1,44 @@
+/* eslint no-unused-vars : 0 */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { withRouter } from 'react-router'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { homePageRoute, notFoundPageRoute } from './route'
+import { addDivWithIdToBody, checkAccessibilityIssues } from './library/startup'
+import { ErrorBoundary } from './pattern/atom/error/ErrorBoundary'
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact={homePageRoute.exact} path={homePageRoute.path} component={homePageRoute.clientComponent} />
+      <Route component={withRouter(notFoundPageRoute.clientComponent)} />
+    </Switch>
+  </BrowserRouter>
+)
+
+addDivWithIdToBody('app')
+checkAccessibilityIssues(React, ReactDOM, 1000)
+
+export function StrictApp() {
+  return (
+    <React.StrictMode>
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </React.StrictMode>
+  )
+}
+
+export function RegularApp() {
+  return (
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  )
+}
+
+ReactDOM.render(<RegularApp />, document.getElementById('app'))
+
+if (module.hot) {
+  module.hot.accept()
+}
