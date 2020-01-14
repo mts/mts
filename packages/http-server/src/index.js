b866fafed566e7f3968726308b09a0c329891c84
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-14 20:20:04 +0100

    remove dev and build run condition depending on DEV_ENV

--- packages/http-server/src/index.js
+++ packages/http-server/src/index.js
@@ -1,11 +1,7 @@
 /* eslint global-require: 0 */
 
-if (process.env.DEV_ENV === 'true') {
-  require('@babel/register')({
-    extensions: ['.js', '.jsx'],
-  })
+require('@babel/register')({
+  extensions: ['.js', '.jsx'],
+})
 
-  require('./server')
-} else {
-  require('../build/server')
-}
+require('./server')
