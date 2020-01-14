938fdafcaecd0d975c524a436fda8a72562147d4
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-14 19:38:50 +0100

    require @babel/register on server

--- packages/http-server/src/index.js
+++ packages/http-server/src/index.js
@@ -1,64 +1,11 @@
-/* eslint no-console: 0 */
-import dotenv from 'dotenv'
-import cors from 'cors'
-import express from 'express'
-import React from 'react'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import { renderToString } from 'react-dom/server'
-// import { RegularApp } from '../../client/src'
+/* eslint global-require: 0 */
 
-function renderFullPage(html, finalState) {
-  return `
-    <!doctype html>
-    <html>
-      <head>
-        <title>Redux Universal Example</title>
-      </head>
-      <body>
-        <div id="app">${html}</div>
-        <script>
-          window.__PRELOADED_STATE__ = ${JSON.stringify(finalState).replace(/</g, '\\u003c')}
-        </script>
-        <script src="/static/bundle.js"></script>
-      </body>
-    </html>
-    `
+if (process.env.DEV_ENV === 'true') {
+  require('@babel/register')({
+    extensions: ['.js', '.jsx'],
+  })
+
+  require('./server')
+} else {
+  require('../build/server')
 }
-
-function someReducer(state = [], action) {
-  switch (action.type) {
-    default:
-      return state
-  }
-}
-
-function handleRender(req, res) {
-  const store = createStore(someReducer, { someState: 'some-state' })
-
-  const html = renderToString(
-    <Provider store={store}>
-      <div>test</div>
-    </Provider>,
-  )
-
-  const finalState = store.getState()
-
-  res.send(renderFullPage(html, finalState))
-}
-
-dotenv.config()
-
-const server = express()
-
-server.use(cors())
-
-server.use('/static', express.static('static'))
-
-server.use(handleRender)
-
-const PORT = process.env.PORT || 3001
-
-server.listen(PORT, () => {
-  console.log(`🚀 MTS HTTP Server listening on port ${PORT}`)
-})
