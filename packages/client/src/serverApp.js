429d02da38e91b3e7de121042eb9cf2d4d3bfe11
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-15 03:01:52 +0100

    use StaticRouter instead of BrowserRouter

--- packages/client/src/serverApp.js
+++ packages/client/src/serverApp.js
@@ -1,18 +1,14 @@
 /* eslint no-unused-vars : 0 */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { withRouter } from 'react-router'
-import { StaticRouter, Route, Switch } from 'react-router-dom'
+import { StaticRouter } from 'react-router'
 import { homePageRoute, notFoundPageRoute } from './route'
 import { ErrorBoundary } from './pattern/atom/error/ErrorBoundary'
 
 function App() {
   return (
     <StaticRouter>
-      <Switch>
-        <Route exact={homePageRoute.exact} path={homePageRoute.path} component={homePageRoute.serverComponent} />
-        <Route component={withRouter(notFoundPageRoute.serverComponent)} />
-      </Switch>
+      <homePageRoute.serverComponent />
     </StaticRouter>
   )
 }
