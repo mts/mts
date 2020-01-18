e75d8a78f8443e1eb8109911b0d60bbc6c10cf94
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-18 16:47:49 +0100

    implement MainContent

--- packages/client/src/pattern/molecule/MainContent/MainContent.jsx
+++ packages/client/src/pattern/molecule/MainContent/MainContent.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import cx from 'classnames'
-import { main, mainBackgroundImage } from './MainContent.scss'
+import {
+  main,
+  mainModule,
+  mainBackground,
+  mainBackgroundDimmer,
+  mainBackgroundCaption,
+  mainBackgroundCaptionPrimaryHeader,
+  mainBackgroundCaptionSecondaryHeader,
+} from './MainContent.scss'
 import { mainContentDefaultProps, mainContentPropTypes } from './MainContent.props'
+import Cover from '../../../asset/image/cover.jpg'
 
 export function MainContent({ className }) {
   return (
-    <div className={cx(className, main)}>
-      <div className={mainBackgroundImage} />
-    </div>
+    <main className={cx(className, main)}>
+      <div className={mainModule}>
+        <div className={mainBackground}>
+          <picture>
+            <img src={Cover} alt="cover" />
+          </picture>
+          <div className={mainBackgroundDimmer} />
+          <div className={mainBackgroundCaption}>
+            <h1 className={mainBackgroundCaptionPrimaryHeader}>MTS</h1>
+            <h2 className={mainBackgroundCaptionSecondaryHeader}>React.js, Node.js and GraphQL</h2>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
