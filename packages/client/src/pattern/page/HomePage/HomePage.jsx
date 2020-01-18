77ef983721dff6f42cfff3df4bdff4cd4ffd9d09
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-18 19:04:27 +0100

    pass on data props to HomeTemplate

--- packages/client/src/pattern/page/HomePage/HomePage.jsx
+++ packages/client/src/pattern/page/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ import { homePageDefaultProps, homePagePropTypes } from './HomePage.props'
 export function HomePage({ className }) {
   return (
     <div className={className}>
-      <HomeTemplate />
+      <HomeTemplate data={{}} />
     </div>
   )
 }
