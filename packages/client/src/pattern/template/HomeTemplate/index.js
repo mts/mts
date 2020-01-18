d705b81ffc18e5f0d25d3d8ac1f788afaab0a545
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-18 19:05:04 +0100

    use index.js for HomeTemplate implementation for server side rendering

--- packages/client/src/pattern/template/HomeTemplate/index.js
+++ packages/client/src/pattern/template/HomeTemplate/index.js
@@ -1,1 +1,19 @@
-export { HomeTemplate } from './HomeTemplate'
+import React from 'react'
+import cx from 'classnames'
+import { homeTemplate } from './HomeTemplate.scss'
+import { Navigation } from '../../molecule/Navigation'
+import { MainContent } from '../../molecule/MainContent'
+import { homeTemplateDefaultProps, homeTemplatePropTypes } from './HomeTemplate.props'
+
+export function HomeTemplate({ className }) {
+  return (
+    <div className={cx(className, homeTemplate)}>
+      <Navigation data={{}} />
+      <MainContent data={{}} />
+    </div>
+  )
+}
+
+HomeTemplate.defaultProps = homeTemplateDefaultProps
+
+HomeTemplate.propTypes = homeTemplatePropTypes
