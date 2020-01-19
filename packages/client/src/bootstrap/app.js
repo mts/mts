30f73111290a0c221f99765c980ba3710f9b6e1d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-19 23:43:58 +0100

    rename appState to appDefaultState

--- packages/client/src/bootstrap/app.js
+++ packages/client/src/bootstrap/app.js
@@ -1,5 +1,5 @@
 export const isRunningOnLocalHost = !process.env.BASE_URL
 
-export const isRunningOnGitHubPages = process.env.BASE_URL === '/mts' && typeof window.appState === 'undefined'
+export const isRunningOnGitHubPages = process.env.BASE_URL === '/mts' && typeof window.appDefaultState === 'undefined'
 
-export const isRunningOnHeroku = process.env.BASE_URL === '/mts' && typeof window.appState !== 'undefined'
+export const isRunningOnHeroku = process.env.BASE_URL === '/mts' && typeof window.appDefaultState !== 'undefined'
