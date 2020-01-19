95e6dee266bf42d7d7599f8535418eb1076c0222
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-19 23:49:05 +0100

    check github and heroku with window.location.href

--- packages/client/src/bootstrap/app.js
+++ packages/client/src/bootstrap/app.js
@@ -1,5 +1,5 @@
 export const isRunningOnLocalHost = !process.env.BASE_URL
 
-export const isRunningOnGitHubPages = process.env.BASE_URL === '/mts' && typeof window.appDefaultState === 'undefined'
+export const isRunningOnGitHubPages = process.env.BASE_URL === '/mts' && window.location.href.includes('mts.github.io/mts')
 
-export const isRunningOnHeroku = process.env.BASE_URL === '/mts' && typeof window.appDefaultState !== 'undefined'
+export const isRunningOnHeroku = process.env.BASE_URL === '/mts' && window.location.href.includes('mts-http-server.herokuapp.com/mts')
