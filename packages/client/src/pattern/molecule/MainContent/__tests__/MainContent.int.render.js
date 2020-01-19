c3b4b3854962bf2913e417dc62b553b06e89da05
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-19 22:07:27 +0100

    correct home page data path

--- packages/client/src/pattern/molecule/MainContent/__tests__/MainContent.int.render.js
+++ packages/client/src/pattern/molecule/MainContent/__tests__/MainContent.int.render.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import { MainContent } from '../index'
+import { homePageData } from '../../../../../../http-server/src/data/page/home'
 
-export const defaultRender = <MainContent />
+export const defaultRender = <MainContent data={homePageData.mainContent} />
--- packages/client/src/pattern/molecule/Navigation/__tests__/Navigation.int.render.js
+++ packages/client/src/pattern/molecule/Navigation/__tests__/Navigation.int.render.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import { Navigation } from '../index'
+import { homePageData } from '../../../../../../http-server/src/data/page/home'
 
-export const defaultRender = <Navigation />
+export const defaultRender = <Navigation data={homePageData.navigation} />
