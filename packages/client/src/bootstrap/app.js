de01777ca93bf845bfd334664ab1f21e3915716c
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-20 00:56:48 +0100

    assert typeof window

--- packages/client/src/bootstrap/app.js
+++ packages/client/src/bootstrap/app.js
@@ -1,3 +1,3 @@
-export const isRunningOnLocalHost = window.location.href.includes('localhost')
+export const isRunningOnLocalHost = typeof window !== 'undefined' && window.location.href.includes('localhost')
 
 export const isRunningOnGitHubPages = process.env.BASE_URL === '/mts' && window.location.href.includes('mts.github.io/mts')
