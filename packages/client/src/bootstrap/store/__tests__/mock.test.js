481edbbd358a8d5252ce663578cd4c7c36e95017
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-19 22:12:22 +0100

    cover getAppMockStore() with unit tests

---
+++ packages/client/src/bootstrap/store/__tests__/mock.test.js
@@ -0,0 +1,23 @@
+import { getAppMockStore } from '../mock'
+
+jest.mock('../../../store/state/mock', () => ({
+  mockAppState: { someData: 'some-data' },
+}))
+
+describe('mock', () => {
+  const customDataObject = { someCustomData: 'some-custom-data' }
+
+  describe('getAppMockStore()', () => {
+    test('must return a store with default data when custom data not provided ', () => {
+      const store = getAppMockStore()
+
+      expect(store.getState()).toMatchObject({ someData: 'some-data' })
+    })
+
+    test('must return a store with custom data when custom data provided', () => {
+      const store = getAppMockStore(customDataObject)
+
+      expect(store.getState()).toMatchObject(customDataObject)
+    })
+  })
+})
