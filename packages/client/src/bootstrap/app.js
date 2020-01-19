af68b326adbd5c3888bb1c3e6d3fcb537ce42dfb
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-20 00:07:00 +0100

    calculate isRunningOnHeroku

--- packages/client/src/bootstrap/app.js
+++ packages/client/src/bootstrap/app.js
@@ -2,4 +2,4 @@ export const isRunningOnLocalHost = !process.env.BASE_URL
 
 export const isRunningOnGitHubPages = process.env.BASE_URL === '/mts' && window.location.href.includes('mts.github.io/mts')
 
-export const isRunningOnHeroku = process.env.BASE_URL === '/mts' && window.location.href.includes('mts-http-server.herokuapp.com/mts')
+export const isRunningOnHeroku = !isRunningOnLocalHost && !isRunningOnGitHubPages
