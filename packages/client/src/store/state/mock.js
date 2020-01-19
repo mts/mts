64f539feb6be3d4afa434e227795e1ca142758a7
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-20 00:56:37 +0100

    manually assign isRunningOnLocalHost and isRunningOnGitHubPages

--- packages/client/src/store/state/mock.js
+++ packages/client/src/store/state/mock.js
@@ -1,5 +1,4 @@
 import { data as appDefaultState } from '../../../../http-server/src/data'
-import { isRunningOnLocalHost, isRunningOnGitHubPages } from '../../bootstrap/app'
 
 const {
   uiData: { homePageData, notFoundPageData },
@@ -7,8 +6,8 @@ const {
 
 export const mockAppState = {
   context: {
-    isRunningOnLocalHost,
-    isRunningOnGitHubPages,
+    isRunningOnLocalHost: true,
+    isRunningOnGitHubPages: false,
     isRunningOnHeroku: false,
   },
   api: {},
