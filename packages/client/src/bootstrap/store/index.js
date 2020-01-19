ae2935b3b350d5b841d3136fd0d5ba7776d011cb
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-19 22:11:03 +0100

    implement store

---
+++ packages/client/src/bootstrap/store/index.js
@@ -0,0 +1,38 @@
+import { applyMiddleware, compose } from 'redux'
+import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
+import { isRunningOnLocalHost } from '../app'
+
+class Store {
+  constructor() {
+    this.setMiddlewares()
+    this.setEnhancer()
+  }
+
+  setMiddlewares() {
+    this.middlewares = [thunk]
+
+    if (isRunningOnLocalHost) {
+      const logger = createLogger({
+        collapsed: true,
+        logger: console,
+      })
+
+      this.middlewares.push(logger)
+    }
+
+    return this.middlewares
+  }
+
+  /* istanbul ignore next */
+  setEnhancer() {
+    const composeEnhancers =
+      typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && isRunningOnLocalHost
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+        : compose
+
+    this.enhancer = composeEnhancers(applyMiddleware(...this.middlewares))
+  }
+}
+
+export { Store }
