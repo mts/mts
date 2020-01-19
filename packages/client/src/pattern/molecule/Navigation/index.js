7fed1035af9eb72636ec4d11022f99779fdb9cdf
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-19 23:02:12 +0100

    connect Navigation

--- packages/client/src/pattern/molecule/Navigation/index.js
+++ packages/client/src/pattern/molecule/Navigation/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import cx from 'classnames'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { nav, navBar, navBarLogo, navBarList, navBarListItem, navBarListItemLink } from './Navigation.scss'
 import { navigationDefaultProps, navigationPropTypes } from './Navigation.props'
 
-export function Navigation({ className }) {
+function Component({ className, ui }) {
+  const {
+    homePageData: {
+      navigation: {
+        navBarList: { about, blog, courses },
+      },
+    },
+  } = ui
+
   return (
     <nav className={cx(className, nav)}>
       <div className={navBar}>
@@ -12,17 +22,17 @@ export function Navigation({ className }) {
         </a>
         <ul className={navBarList}>
           <li className={navBarListItem}>
-            <a className={navBarListItemLink} href="/about" aria-label="About Page">
+            <a className={navBarListItemLink} href={about} aria-label="About Page">
               About
             </a>
           </li>
           <li className={navBarListItem}>
-            <a className={navBarListItemLink} href="/blog" aria-label="Blog Page">
+            <a className={navBarListItemLink} href={blog} aria-label="Blog Page">
               Blog
             </a>
           </li>
           <li className={navBarListItem}>
-            <a className={navBarListItemLink} href="/courses" aria-label="Courses Page">
+            <a className={navBarListItemLink} href={courses} aria-label="Courses Page">
               Courses
             </a>
           </li>
@@ -32,6 +42,15 @@ export function Navigation({ className }) {
   )
 }
 
-Navigation.defaultProps = navigationDefaultProps
+Component.defaultProps = navigationDefaultProps
 
-Navigation.propTypes = navigationPropTypes
+Component.propTypes = navigationPropTypes
+
+export const Navigation = connect(
+  ({ context, api, ui }) => ({
+    context,
+    api,
+    ui,
+  }),
+  dispatch => bindActionCreators({}, dispatch),
+)(Component)
