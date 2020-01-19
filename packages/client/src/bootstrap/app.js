e4898bbb303be5b0fb337145f387375daa0856e9
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-20 00:29:59 +0100

    remove isRunningOnHeroku

--- packages/client/src/bootstrap/app.js
+++ packages/client/src/bootstrap/app.js
@@ -1,5 +1,3 @@
 export const isRunningOnLocalHost = !process.env.BASE_URL
 
 export const isRunningOnGitHubPages = process.env.BASE_URL === '/mts' && window.location.href.includes('mts.github.io/mts')
-
-export const isRunningOnHeroku = isRunningOnLocalHost ? false : !isRunningOnGitHubPages
