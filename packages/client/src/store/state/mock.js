58d230092a2acacda1a9724212aee542ecf9e195
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-21 12:21:06 +0100

    use abstract out getAppDefaultState() in state mock

--- packages/client/src/store/state/mock.js
+++ packages/client/src/store/state/mock.js
@@ -1,18 +1,7 @@
-import { data as appDefaultState } from '../../../../http-server/src/data'
+import { getAppDefaultState } from '../../../../library/state'
 
-const {
-  uiData: { homePageData, notFoundPageData },
-} = appDefaultState
-
-export const mockAppState = {
-  context: {
-    isRunningOnLocalHost: true,
-    isRunningOnGitHubPages: false,
-    isRunningOnHeroku: false,
-  },
-  api: {},
-  ui: {
-    homePageData,
-    notFoundPageData,
-  },
-}
+export const mockAppState = getAppDefaultState({
+  isRunningOnLocalHostOverwrite: true,
+  isRunningOnGitHubPagesOverwrite: false,
+  isRunningOnHeroku: false,
+})
