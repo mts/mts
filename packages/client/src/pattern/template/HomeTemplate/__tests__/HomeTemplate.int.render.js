9215e22a60818e3a62c9c9b9fa383789116446fe
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-21 23:39:43 +0100

    update HomeTemplate snapshots

--- packages/client/src/pattern/template/HomeTemplate/__tests__/HomeTemplate.int.render.js
+++ packages/client/src/pattern/template/HomeTemplate/__tests__/HomeTemplate.int.render.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { HomeTemplate } from '../index'
 import { getAppMockStore } from '../../../../store/mock'
-import { mockAppState } from '../../../../store/state/mock'
+import { mockAppState } from '../../../../../../library/src/state/mock'
 
 export const defaultRender = (
   <Provider store={getAppMockStore(mockAppState)}>
