43be2517c3d900a3889d60eb89149afa4b7627bd
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-21 23:39:29 +0100

    update Navigation snapshots

--- packages/client/src/pattern/molecule/Navigation/__tests__/Navigation.int.render.js
+++ packages/client/src/pattern/molecule/Navigation/__tests__/Navigation.int.render.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { Navigation } from '../index'
 import { getAppMockStore } from '../../../../store/mock'
-import { mockAppState } from '../../../../store/state/mock'
+import { mockAppState } from '../../../../../../library/src/state/mock'
 
 export const defaultRender = (() => {
   return (
