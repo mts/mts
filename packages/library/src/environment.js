baa5e4e1173c295bc60ca6b913a695322b36555a
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-21 23:40:15 +0100

    add environment

--- packages/library/src/environment.js
+++ packages/library/src/environment.js
@@ -1,3 +1,7 @@
-export const isRunningOnLocalHost = typeof window !== 'undefined' && window.location.href.includes('localhost')
+export const isRunningOnLocalHostViaPort = process.env.PORT === '3000'
+
+export const isRunningOnLocalHostViaDomain = typeof window !== 'undefined' && window.location.href.includes('localhost')
 
 export const isRunningOnGitHubPages = process.env.BASE_URL === '/mts' && window.location.href.includes('mts.github.io/mts')
+
+export const windowObjectExists = typeof window !== 'undefined'
