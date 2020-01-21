da2be8c7ec98c8a247e701776196dc20c6a4992b
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-21 22:14:23 +0100

    add state mock

--- packages/client/src/store/state/mock.js
+++
@@ -1,7 +0,0 @@
-import { getAppDefaultState } from '../../../../library/src/state'
-
-export const mockAppState = getAppDefaultState({
-  isRunningOnLocalHostOverwrite: true,
-  isRunningOnGitHubPagesOverwrite: false,
-  isRunningOnHeroku: false,
-})
