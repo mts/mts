e76fb89493cfe12efe3c778339bb4742688f279d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-21 12:20:44 +0100

    use abstract out getAppDefaultState() in client

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,8 +1,8 @@
 /* eslint global-require: 0 */
 
 import './index.scss'
-import { data as appDefaultState } from '../../http-server/src/data'
 import { isRunningOnLocalHost, isRunningOnGitHubPages } from '../../library/environment'
+import { getAppDefaultState } from '../../library/state'
 
 require('@babel/register')({
   extensions: ['.js', '.jsx'],
@@ -12,23 +12,12 @@ require('@babel/register')({
 if (typeof window === 'undefined') {
   require('./serverRenderApp')
 } else {
-  const {
-    uiData: { homePageData, notFoundPageData },
-  } = appDefaultState
-
   if (isRunningOnLocalHost || isRunningOnGitHubPages) {
-    window.appDefaultState = {
-      context: {
-        isRunningOnLocalHost,
-        isRunningOnGitHubPages,
-        isRunningOnHeroku: false,
-      },
-      api: {},
-      ui: {
-        homePageData,
-        notFoundPageData,
-      },
-    }
+    window.appDefaultState = getAppDefaultState({
+      isRunningOnLocalHostOverwrite: isRunningOnLocalHost,
+      isRunningOnGitHubPagesOverwrite: isRunningOnGitHubPages,
+      isRunningOnHeroku: false,
+    })
   }
 
   require('./clientRenderApp')
