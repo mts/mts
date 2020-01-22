59c8d06b7d807c642fed4ab3844bee8f10c73fae
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-22 12:21:55 +0100

    add action types and actions

---
+++ packages/client/src/store/action/actionCreators.js
@@ -0,0 +1,3 @@
+import { API_REST_GITHUB_SET_MTS_REPOSITORIES } from './actionTypes'
+
+export const apiRestGitHubSetMTSRepositoriesAction = payload => ({ type: API_REST_GITHUB_SET_MTS_REPOSITORIES, payload })
