139dd6bf2883b04a272c47caf8ea7b9c153e07f5
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-22 18:06:36 +0100

    cover apiGraphQLGitHubSetMTSRepositoriesAction with unit test

--- packages/client/src/store/action/__tests__/actionCreators.test.js
+++ packages/client/src/store/action/__tests__/actionCreators.test.js
@@ -1,4 +1,4 @@
-import { apiRestGitHubSetMTSRepositoriesAction } from '../actionCreators'
+import { apiRestGitHubSetMTSRepositoriesAction, apiGraphQLGitHubSetMTSRepositoriesAction } from '../actionCreators'
 
 describe('actionCreators', () => {
   const payload = { somePayload: 'some-payload' }
@@ -6,6 +6,12 @@ describe('actionCreators', () => {
   describe('apiRestGitHubSetMTSRepositoriesAction()', () => {
     test('must return action with given payload', () => {
       expect(apiRestGitHubSetMTSRepositoriesAction(payload)).toMatchObject({ type: 'API_REST_GITHUB_SET_MTS_REPOSITORIES', payload })
+    })
+  })
+
+  describe('apiGraphQLGitHubSetMTSRepositoriesAction()', () => {
+    test('must return action with given payload', () => {
+      expect(apiGraphQLGitHubSetMTSRepositoriesAction(payload)).toMatchObject({ type: 'API_GRAPHQL_GITHUB_SET_MTS_REPOSITORIES', payload })
     })
   })
 })
