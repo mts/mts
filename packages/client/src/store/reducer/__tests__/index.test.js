7abb63945f0ec411536abfe864f0b77ef4bbcff5
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-22 13:51:57 +0100

    cover reducer with unit tests

---
+++ packages/client/src/store/reducer/__tests__/index.test.js
@@ -0,0 +1,33 @@
+import { apiRestGitHubSetMTSRepositoriesAction } from '../../action/actionCreators'
+import { mockAppState } from '../../../../../library/src/state/mock'
+
+jest.mock('../../../../../library/src/state/default', () => ({
+  getAppDefaultStateFromWindow: () => ({}),
+  getAppDefaultStateFromData: () => ({}),
+}))
+
+const stateDefaultMockObject = require('../../../../../library/src/state/default')
+
+describe('index', () => {
+  beforeEach(() => {
+    jest.spyOn(stateDefaultMockObject, 'getAppDefaultStateFromWindow').mockReturnValue(mockAppState)
+  })
+
+  afterEach(() => jest.clearAllMocks())
+
+  describe('getAppReducer()', () => {
+    describe('api state', () => {
+      test('must return new state with given payload when API_REST_GITHUB_SET_MTS_REPOSITORIES action dispatched', async () => {
+        const payload = [
+          { id: 1, name: 'some-name' },
+          { id: 2, name: 'some-other-name' },
+        ]
+
+        const { clientAppStore } = await import('../../index')
+
+        clientAppStore.dispatch(apiRestGitHubSetMTSRepositoriesAction(payload))
+        expect(clientAppStore.getState().api.rest.github.MTSRepos).toMatchObject(payload)
+      })
+    })
+  })
+})
