38dc0cd1cf676df2c183eb6205215923fc7955d2
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-22 18:04:05 +0100

    extend setAppCompleteStateThunk()

--- packages/client/src/store/thunk/__tests__/completeState.test.js
+++ packages/client/src/store/thunk/__tests__/completeState.test.js
@@ -2,10 +2,10 @@ import { setAppCompleteStateThunk } from '../completeState'
 import { mockAppState } from '../../../../../library/src/state/mock'
 
 jest.mock('../../action/actionCreators', () => ({ apiRestGitHubSetMTSRepositoriesAction: () => {} }))
-jest.mock('../../../api/rest/github/users', () => ({ getUsersMTSRepos: () => {} }))
+jest.mock('../../../api/rest/github/users', () => ({ getUsersMTSReposRest: () => {} }))
 
 const actionCreatorsMockObject = require('../../action/actionCreators')
-const getUsersMTSReposMockObject = require('../../../api/rest/github/users')
+const getUsersMTSReposRestMockObject = require('../../../api/rest/github/users')
 
 describe('completeState', () => {
   const actionDataObject = { someActionData: 'some-action-data' }
@@ -16,28 +16,28 @@ describe('completeState', () => {
 
   const dispatchMockFn = jest.fn()
   const apiRestGitHubSetMTSRepositoriesActionMockFn = jest.fn(() => actionDataObject)
-  const getUsersMTSReposMockFn = jest.fn(() => Promise.resolve(responseDataObject))
+  const getUsersMTSReposRestMockFn = jest.fn(() => Promise.resolve(responseDataObject))
 
   const expectEachMockFnToHaveBeenCalledOnce = async () => {
     await expect(apiRestGitHubSetMTSRepositoriesActionMockFn).toHaveBeenCalledTimes(1)
-    await expect(getUsersMTSReposMockFn).toHaveBeenCalledTimes(1)
+    await expect(getUsersMTSReposRestMockFn).toHaveBeenCalledTimes(1)
   }
 
   beforeEach(() => {
     jest
       .spyOn(actionCreatorsMockObject, 'apiRestGitHubSetMTSRepositoriesAction')
       .mockImplementation(apiRestGitHubSetMTSRepositoriesActionMockFn)
-    jest.spyOn(getUsersMTSReposMockObject, 'getUsersMTSRepos').mockImplementation(getUsersMTSReposMockFn)
+    jest.spyOn(getUsersMTSReposRestMockObject, 'getUsersMTSReposRest').mockImplementation(getUsersMTSReposRestMockFn)
   })
 
   afterEach(() => jest.clearAllMocks())
 
   describe('setAppCompleteStateThunk()', () => {
-    test('must dispatch apiRestGitHubSetMTSRepositoriesAction() action with getUsersMTSRepos() response data', async () => {
+    test('must dispatch apiRestGitHubSetMTSRepositoriesAction() action with getUsersMTSReposRest() response data', async () => {
       await setAppCompleteStateThunk(mockAppState)(dispatchMockFn)
 
       expectEachMockFnToHaveBeenCalledOnce()
-      await expect(getUsersMTSReposMockFn).toHaveBeenCalled()
+      await expect(getUsersMTSReposRestMockFn).toHaveBeenCalled()
       await expect(apiRestGitHubSetMTSRepositoriesActionMockFn).toHaveBeenCalledWith(responseDataObject)
       await expect(dispatchMockFn).toHaveBeenCalledWith(actionDataObject)
     })
--- packages/client/src/store/thunk/completeState.js
+++ packages/client/src/store/thunk/completeState.js
@@ -1,11 +1,13 @@
-import { apiRestGitHubSetMTSRepositoriesAction } from '../action/actionCreators'
-import { getUsersMTSRepos } from '../../api/rest/github/users'
+import { apiRestGitHubSetMTSRepositoriesAction, apiGraphQLGitHubSetMTSRepositoriesAction } from '../action/actionCreators'
+import { getUsersMTSReposRest } from '../../api/rest/github/users'
+import { getUsersMTSReposGraphQL } from '../../api/graphql/github/users'
 
 export const setAppCompleteStateThunk = () => async dispatch => {
-  const responseData = await getUsersMTSRepos()
-  const refinedResponseData = responseData.map(({ id, name }) => {
-    return { id, name }
-  })
+  const restResponseData = await getUsersMTSReposRest()
+  const refinedRestResponseData = restResponseData.map(({ id, name }) => ({ id, name }))
+  dispatch(apiRestGitHubSetMTSRepositoriesAction(refinedRestResponseData))
 
-  dispatch(apiRestGitHubSetMTSRepositoriesAction(refinedResponseData))
+  const graphqlResponseData = await getUsersMTSReposGraphQL()
+  const refinedGraphqlResponseData = graphqlResponseData.data.user.repositories.nodes.map(({ id, name }) => ({ id, name }))
+  dispatch(apiGraphQLGitHubSetMTSRepositoriesAction(refinedGraphqlResponseData))
 }
