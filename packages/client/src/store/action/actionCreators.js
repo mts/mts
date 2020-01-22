81794453b8f2b74648002b5bf7fb3eaa290e718a
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-22 17:59:57 +0100

    extend actions

--- packages/client/src/store/action/actionCreators.js
+++ packages/client/src/store/action/actionCreators.js
@@ -1,3 +1,4 @@
-import { API_REST_GITHUB_SET_MTS_REPOSITORIES } from './actionTypes'
+import { API_REST_GITHUB_SET_MTS_REPOSITORIES, API_GRAPHQL_GITHUB_SET_MTS_REPOSITORIES } from './actionTypes'
 
 export const apiRestGitHubSetMTSRepositoriesAction = payload => ({ type: API_REST_GITHUB_SET_MTS_REPOSITORIES, payload })
+export const apiGraphQLGitHubSetMTSRepositoriesAction = payload => ({ type: API_GRAPHQL_GITHUB_SET_MTS_REPOSITORIES, payload })
