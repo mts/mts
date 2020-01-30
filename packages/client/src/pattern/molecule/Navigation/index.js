a026709493ae8aa7684877edab895b391cde496d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-01-30 22:04:35 +0100

    toggle render navBarList and navBarLogo for mobile and desktop

--- packages/client/src/pattern/molecule/Navigation/index.js
+++ packages/client/src/pattern/molecule/Navigation/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import cx from 'classnames'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
@@ -7,6 +7,7 @@ import {
   navBar,
   navBarLogo,
   navBarList,
+  navBarListToggle,
   navBarListItem,
   navBarListItemLink,
   navBarMobileToggle,
@@ -15,8 +16,16 @@ import {
 import { navigationDefaultProps, navigationPropTypes } from './Navigation.props'
 import { windowObjectExists } from '../../../../../library/src/environment'
 import Logo from '../../../asset/image/logo.jpg'
+import { handleStickyNavBar } from '../../../../../library/src/client/navBar'
+import { isMobile } from '../../../../../library/src/client/window'
 
 function Component({ className, ui }) {
+  const [isNavBarListToggled, toggleNavBarList] = useState(false)
+
+  useEffect(() => {
+    handleStickyNavBar()
+  })
+
   const {
     homePageData: {
       navigation: {
@@ -28,10 +37,21 @@ function Component({ className, ui }) {
   return (
     <nav className={cx(className, nav)}>
       <div className={navBar}>
-        <a className={navBarLogo} href="/">
-          <img height="40" src={!windowObjectExists ? 'mts/static/logo.jpg' : Logo} alt="logo" />
-        </a>
-        <ul className={navBarList}>
+        {isMobile && isNavBarListToggled ? null : null}
+
+        {isMobile && !isNavBarListToggled ? (
+          <a className={navBarLogo} href="/">
+            <img height="40" src={!windowObjectExists ? 'mts/static/logo.jpg' : Logo} alt="logo" />
+          </a>
+        ) : null}
+
+        {!isMobile ? (
+          <a className={navBarLogo} href="/">
+            <img height="40" src={!windowObjectExists ? 'mts/static/logo.jpg' : Logo} alt="logo" />
+          </a>
+        ) : null}
+
+        <ul className={cx(navBarList, isNavBarListToggled ? navBarListToggle : '')}>
           <li className={navBarListItem}>
             <a className={navBarListItemLink} href={about} aria-label="About Page">
               About
@@ -49,7 +69,7 @@ function Component({ className, ui }) {
           </li>
         </ul>
         <div className={navBarMobileToggle}>
-          <button type="button" className={navBarMobileToggleButton}>
+          <button className={navBarMobileToggleButton} onClick={() => toggleNavBarList(!isNavBarListToggled)} type="button">
             <svg width="24px" height="24px" viewBox="0 0 124 124">
               <path d="M112 6H12C5.4 6 0 11.4 0 18s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12zM112 50H12C5.4 50 0 55.4 0 62s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12zM112 94H12c-6.6 0-12 5.4-12 12s5.4 12 12 12h100c6.6 0 12-5.4 12-12s-5.4-12-12-12z" />
             </svg>
