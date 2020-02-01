8a240ef72245f3abe80fda4d40c1ee7b0d7c25f7
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-01 16:50:34 +0100

    extend BlogTemplate with Navigation

--- packages/client/src/pattern/template/BlogTemplate/index.js
+++ packages/client/src/pattern/template/BlogTemplate/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import cx from 'classnames'
 import { blogTemplate } from './BlogTemplate.scss'
 import { ShareButtons } from '../../molecule/ShareButtons'
+import { Navigation } from '../../molecule/Navigation'
 import { blogTemplateDefaultProps, blogTemplatePropTypes } from './BlogTemplate.props'
 
 export function BlogTemplate({ className }) {
   return (
     <div className={cx(className, blogTemplate)}>
       <ShareButtons />
+      <Navigation />
     </div>
   )
 }
