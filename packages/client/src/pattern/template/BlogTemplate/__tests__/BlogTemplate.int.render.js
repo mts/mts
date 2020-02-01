5ca9f797e1755b5b0b25d2e3f2d6b6c16f048135
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-01 16:51:44 +0100

    restore BlogTemplate render provide mock store with mock app state

--- packages/client/src/pattern/template/BlogTemplate/__tests__/BlogTemplate.int.render.js
+++ packages/client/src/pattern/template/BlogTemplate/__tests__/BlogTemplate.int.render.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { BlogTemplate } from '../index'
+import { getAppMockStore } from '../../../../../../library/src/store/mock'
+import { mockAppState } from '../../../../../../library/src/state/mock'
 
-export const defaultRender = <BlogTemplate />
+export const defaultRender = (
+  <Provider store={getAppMockStore(mockAppState)}>
+    <BlogTemplate />
+  </Provider>
+)
