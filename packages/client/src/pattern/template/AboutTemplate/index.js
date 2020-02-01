381dadbf58226075dba5d319705f66d77a7b1a0e
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-01 17:07:25 +0100

    extend AboutTemplate with ShareButtons

--- packages/client/src/pattern/template/AboutTemplate/index.js
+++ packages/client/src/pattern/template/AboutTemplate/index.js
@@ -2,9 +2,14 @@ import React from 'react'
 import cx from 'classnames'
 import { aboutTemplate } from './AboutTemplate.scss'
 import { aboutTemplateDefaultProps, aboutTemplatePropTypes } from './AboutTemplate.props'
+import { ShareButtons } from '../../molecule/ShareButtons'
 
 export function AboutTemplate({ className }) {
-  return <div className={cx(className, aboutTemplate)}>AboutTemplate under construction</div>
+  return (
+    <div className={cx(className, aboutTemplate)}>
+      <ShareButtons />
+    </div>
+  )
 }
 
 AboutTemplate.defaultProps = aboutTemplateDefaultProps
