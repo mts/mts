50a8d0a141b75edd9da6900b76dacbf743738d85
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-01 13:46:16 +0100

    remove AboutPage render provide mock store with mock app state

--- packages/client/src/pattern/page/AboutPage/__tests__/AboutPage.int.render.js
+++ packages/client/src/pattern/page/AboutPage/__tests__/AboutPage.int.render.js
@@ -1,18 +1,7 @@
 import React from 'react'
-import { Provider } from 'react-redux'
 import { AsyncAboutPage } from '../AsyncAboutPage'
 import { AboutPage } from '../AboutPage'
-import { getAppMockStore } from '../../../../../../library/src/store/mock'
-import { mockAppState } from '../../../../../../library/src/state/mock'
 
-export const asyncRender = (
-  <Provider store={getAppMockStore(mockAppState)}>
-    <AsyncAboutPage />
-  </Provider>
-)
+export const asyncRender = <AsyncAboutPage />
 
-export const defaultRender = (
-  <Provider store={getAppMockStore(mockAppState)}>
-    <AboutPage />
-  </Provider>
-)
+export const defaultRender = <AboutPage />
