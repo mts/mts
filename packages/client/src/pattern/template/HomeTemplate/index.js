c95054a473e2a9b7c595c11556b40f417732e242
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-01 17:53:50 +0100

    import molecules last

--- packages/client/src/pattern/template/HomeTemplate/index.js
+++ packages/client/src/pattern/template/HomeTemplate/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import cx from 'classnames'
 import { homeTemplate } from './HomeTemplate.scss'
+import { homeTemplateDefaultProps, homeTemplatePropTypes } from './HomeTemplate.props'
 import { ShareButtons } from '../../molecule/ShareButtons'
 import { Navigation } from '../../molecule/Navigation'
 import { HomeMain } from '../../molecule/HomeMain'
 import { Newsletter } from '../../molecule/Newsletter'
 import { Footer } from '../../molecule/Footer'
-import { homeTemplateDefaultProps, homeTemplatePropTypes } from './HomeTemplate.props'
 
 export function HomeTemplate({ className }) {
   return (
