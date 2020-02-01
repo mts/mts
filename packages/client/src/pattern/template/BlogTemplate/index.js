0d114bbf3d24ec8fcda1ceb70a99d8fd71ef5d09
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-01 16:36:25 +0100

    extend BlogTemplate with ShareButtons

--- packages/client/src/pattern/template/BlogTemplate/index.js
+++ packages/client/src/pattern/template/BlogTemplate/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import cx from 'classnames'
 import { blogTemplate } from './BlogTemplate.scss'
+import { ShareButtons } from '../../molecule/ShareButtons'
 import { blogTemplateDefaultProps, blogTemplatePropTypes } from './BlogTemplate.props'
 
 export function BlogTemplate({ className }) {
-  return <div className={cx(className, blogTemplate)}>BlogTemplate under construction</div>
+  return (
+    <div className={cx(className, blogTemplate)}>
+      <ShareButtons />
+    </div>
+  )
 }
 
 BlogTemplate.defaultProps = blogTemplateDefaultProps
