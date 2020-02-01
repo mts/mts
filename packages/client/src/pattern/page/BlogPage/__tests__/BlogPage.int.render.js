fdb1505bcf207b31ae7974373124621ee7046110
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-01 13:46:29 +0100

    remove BlogPage render provide mock store with mock app state

--- packages/client/src/pattern/page/BlogPage/__tests__/BlogPage.int.render.js
+++ packages/client/src/pattern/page/BlogPage/__tests__/BlogPage.int.render.js
@@ -1,18 +1,7 @@
 import React from 'react'
-import { Provider } from 'react-redux'
 import { AsyncBlogPage } from '../AsyncBlogPage'
 import { BlogPage } from '../BlogPage'
-import { getAppMockStore } from '../../../../../../library/src/store/mock'
-import { mockAppState } from '../../../../../../library/src/state/mock'
 
-export const asyncRender = (
-  <Provider store={getAppMockStore(mockAppState)}>
-    <AsyncBlogPage />
-  </Provider>
-)
+export const asyncRender = <AsyncBlogPage />
 
-export const defaultRender = (
-  <Provider store={getAppMockStore(mockAppState)}>
-    <BlogPage />
-  </Provider>
-)
+export const defaultRender = <BlogPage />
