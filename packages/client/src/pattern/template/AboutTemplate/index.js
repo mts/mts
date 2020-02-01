bd2d3fa950ba75982508be0e2e7f161ab520968d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-01 17:08:02 +0100

    extend AboutTemplate with Navigation

--- packages/client/src/pattern/template/AboutTemplate/index.js
+++ packages/client/src/pattern/template/AboutTemplate/index.js
@@ -3,11 +3,13 @@ import cx from 'classnames'
 import { aboutTemplate } from './AboutTemplate.scss'
 import { aboutTemplateDefaultProps, aboutTemplatePropTypes } from './AboutTemplate.props'
 import { ShareButtons } from '../../molecule/ShareButtons'
+import { Navigation } from '../../molecule/Navigation'
 
 export function AboutTemplate({ className }) {
   return (
     <div className={cx(className, aboutTemplate)}>
       <ShareButtons />
+      <Navigation />
     </div>
   )
 }
