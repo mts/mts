0d5b87715e4f78f96dec02211821e33cc64aa50f
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-01 17:08:40 +0100

    extend AboutTemplate with Newsletter

--- packages/client/src/pattern/template/AboutTemplate/index.js
+++ packages/client/src/pattern/template/AboutTemplate/index.js
@@ -4,12 +4,14 @@ import { aboutTemplate } from './AboutTemplate.scss'
 import { aboutTemplateDefaultProps, aboutTemplatePropTypes } from './AboutTemplate.props'
 import { ShareButtons } from '../../molecule/ShareButtons'
 import { Navigation } from '../../molecule/Navigation'
+import { Newsletter } from '../../molecule/Newsletter'
 
 export function AboutTemplate({ className }) {
   return (
     <div className={cx(className, aboutTemplate)}>
       <ShareButtons />
       <Navigation />
+      <Newsletter />
     </div>
   )
 }
