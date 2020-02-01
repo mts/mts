4fd78bdcf7726051becf6c3e3b2336f288aca9e9
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-01 17:06:57 +0100

    import molecules last

--- packages/client/src/pattern/template/BlogTemplate/index.js
+++ packages/client/src/pattern/template/BlogTemplate/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import cx from 'classnames'
 import { blogTemplate } from './BlogTemplate.scss'
+import { blogTemplateDefaultProps, blogTemplatePropTypes } from './BlogTemplate.props'
 import { ShareButtons } from '../../molecule/ShareButtons'
 import { Navigation } from '../../molecule/Navigation'
 import { Newsletter } from '../../molecule/Newsletter'
 import { Footer } from '../../molecule/Footer'
-import { blogTemplateDefaultProps, blogTemplatePropTypes } from './BlogTemplate.props'
 
 export function BlogTemplate({ className }) {
   return (
