f635eecca86ed8627e78ba49d4cc0f5ad61cdcb9
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-01 17:04:50 +0100

    restore AboutTemplate render provide mock store with mock app state

--- packages/client/src/pattern/template/AboutTemplate/__tests__/AboutTemplate.int.render.js
+++ packages/client/src/pattern/template/AboutTemplate/__tests__/AboutTemplate.int.render.js
@@ -1,4 +1,11 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { AboutTemplate } from '../index'
+import { getAppMockStore } from '../../../../../../library/src/store/mock'
+import { mockAppState } from '../../../../../../library/src/state/mock'
 
-export const defaultRender = <AboutTemplate />
+export const defaultRender = (
+  <Provider store={getAppMockStore(mockAppState)}>
+    <AboutTemplate />
+  </Provider>
+)
