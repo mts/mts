71fb575d34f675835cbe3538d1a9aaca9d26e4be
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-02 13:42:45 +0100

    add title, values and mission

--- packages/client/src/pattern/molecule/AboutMain/index.js
+++ packages/client/src/pattern/molecule/AboutMain/index.js
@@ -8,6 +8,7 @@ import {
   container1HeaderFollowButtonsItem,
   container1HeaderFollowButtonsItemFacebookFollow,
   container1HeaderContainer1,
+  container1Title,
 } from './AboutMain.scss'
 import { aboutMainDefaultProps, aboutMainPropTypes } from './AboutMain.props'
 import { handleStickyNavBar } from '../../../../../library/src/client/navBar'
@@ -89,6 +90,48 @@ export function AboutMain({ className }) {
           </div>
           <meta itemProp="image" content={!windowObjectExists ? 'mts/static/banner.jpg' : Banner} />
         </header>
+        <meta itemProp="mainEntityOfPage" content="https://mts.github.io/about" />
+        <arcticle itemProp="articleBody">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec turpis eleifend, pretium leo ut, euismod sapien. Sed quis
+            purus quis metus sagittis sollicitudin accumsan in ante. Cras tortor nisi, vulputate in pharetra et, cursus non felis.
+            Pellentesque libero nulla, auctor ac viverra eu, gravida eu odio.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec turpis eleifend, pretium leo ut, euismod sapien. Sed quis
+            purus quis metus sagittis sollicitudin accumsan in ante. Cras tortor nisi, vulputate in pharetra et, cursus non felis.
+            Pellentesque libero nulla, auctor ac viverra eu, gravida eu odio. Vestibulum dolor ex, mollis eget euismod et, auctor ut est.
+            Etiam posuere hendrerit tincidunt. Curabitur commodo, sapien id mattis sagittis, lectus dolor ullamcorper purus, et lacinia elit
+            urna tincidunt massa. Nam pretium nisi at vehicula placerat. Ut molestie eros in sapien mollis, tincidunt vehicula arcu
+            pellentesque. Proin vel elementum ante. Curabitur ut erat massa. Integer pellentesque mauris sed libero scelerisque molestie.
+            Morbi ac dapibus urna. Donec magna felis, porta eget felis vel, sodales fringilla quam. Maecenas mauris ligula, tempus et
+            pretium quis, aliquam nec odio. Praesent mattis a leo lacinia facilisis. Integer rutrum ac purus nec blandit. Nullam et felis ut
+            eros sodales laoreet non vel ex. Suspendisse quis viverra purus. Pellentesque lobortis semper laoreet. Proin venenatis lacus nec
+            neque aliquam cursus. Nullam porttitor a nulla a venenatis. Duis eu sapien velit. Phasellus sagittis vitae ligula eget posuere.
+            Nullam volutpat cursus nunc, at molestie tellus pellentesque non. Vestibulum dignissim ante vel diam tincidunt iaculis.
+            Vestibulum vehicula lobortis ultrices.
+          </p>
+          <h2 className={container1Title}>
+            <a href="#values-and-mission" target="_blank" rel="noopener noreferrer" aria-label="values and mission permalink">
+              <svg height="0" width="0" />
+            </a>
+            Values and Mission
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec turpis eleifend, pretium leo ut, euismod sapien. Sed quis
+            purus quis metus sagittis sollicitudin accumsan in ante. Cras tortor nisi, vulputate in pharetra et, cursus non felis.
+            Pellentesque libero nulla, auctor ac viverra eu, gravida eu odio. Vestibulum dolor ex, mollis eget euismod et, auctor ut est.
+            Etiam posuere hendrerit tincidunt. Curabitur commodo, sapien id mattis sagittis, lectus dolor ullamcorper purus, et lacinia elit
+            urna tincidunt massa. Nam pretium nisi at vehicula placerat. Ut molestie eros in sapien mollis, tincidunt vehicula arcu
+            pellentesque. Proin vel elementum ante. Curabitur ut erat massa. Integer pellentesque mauris sed libero scelerisque molestie.
+            Morbi ac dapibus urna. Donec magna felis, porta eget felis vel, sodales fringilla quam. Maecenas mauris ligula, tempus et
+            pretium quis, aliquam nec odio. Praesent mattis a leo lacinia facilisis. Integer rutrum ac purus nec blandit. Nullam et felis ut
+            eros sodales laoreet non vel ex. Suspendisse quis viverra purus. Pellentesque lobortis semper laoreet. Proin venenatis lacus nec
+            neque aliquam cursus. Nullam porttitor a nulla a venenatis. Duis eu sapien velit. Phasellus sagittis vitae ligula eget posuere.
+            Nullam volutpat cursus nunc, at molestie tellus pellentesque non. Vestibulum dignissim ante vel diam tincidunt iaculis.
+            Vestibulum vehicula lobortis ultrices.
+          </p>
+        </arcticle>
       </section>
     </main>
   )
