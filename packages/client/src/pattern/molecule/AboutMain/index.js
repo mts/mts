9a7e22de1cf427fb6b41b264b8d00d513892b69b
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-02 13:00:48 +0100

    implement HomeMain header follow buttons

--- packages/client/src/pattern/molecule/AboutMain/index.js
+++ packages/client/src/pattern/molecule/AboutMain/index.js
@@ -1,5 +1,13 @@
 import React, { useEffect } from 'react'
-import { container1, container1Header, container1HeaderSubHeader, container1HeaderSubHeaderPrimary } from './AboutMain.scss'
+import {
+  container1,
+  container1Header,
+  container1HeaderSubHeader,
+  container1HeaderSubHeaderPrimary,
+  container1HeaderFollowButtons,
+  container1HeaderFollowButtonsItem,
+  container1HeaderFollowButtonsItemFacebookFollow,
+} from './AboutMain.scss'
 import { aboutMainDefaultProps, aboutMainPropTypes } from './AboutMain.props'
 import { handleStickyNavBar } from '../../../../../library/src/client/navBar'
 
@@ -47,6 +55,29 @@ export function AboutMain({ className }) {
               </span>
             </div>
           </span>
+          <ul className={container1HeaderFollowButtons}>
+            <li className={container1HeaderFollowButtonsItem}>
+              <a href="https://twitter.com/hashtag/willsmith" target="_blank" rel="noopener noreferrer">
+                <img
+                  src="https://img.shields.io/twitter/follow/willsmith.svg?style=social&amp;label=Follow on Twitter"
+                  alt="Follow on Twitter"
+                />
+              </a>
+            </li>
+            <li className={container1HeaderFollowButtonsItem}>
+              <a
+                href="https://www.facebook.com/Will-Smith-92304305160/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={container1HeaderFollowButtonsItemFacebookFollow}
+              >
+                <svg width="12px" height="12px" viewBox="0 0 96.124 96.123">
+                  <path d="M72.089.02L59.624 0C45.62 0 36.57 9.285 36.57 23.656v10.907H24.037a1.96 1.96 0 0 0-1.96 1.961v15.803a1.96 1.96 0 0 0 1.96 1.96H36.57v39.876a1.96 1.96 0 0 0 1.96 1.96h16.352a1.96 1.96 0 0 0 1.96-1.96V54.287h14.654a1.96 1.96 0 0 0 1.96-1.96l.006-15.803a1.963 1.963 0 0 0-1.961-1.961H56.842v-9.246c0-4.444 1.059-6.7 6.848-6.7l8.397-.003a1.96 1.96 0 0 0 1.959-1.96V1.98A1.96 1.96 0 0 0 72.089.02z" />
+                </svg>
+                &nbsp;Follow on Facebook
+              </a>
+            </li>
+          </ul>
         </header>
       </section>
     </main>
