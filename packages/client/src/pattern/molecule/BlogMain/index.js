21cf106a970fb208b0f1b730feecc27ae0cfeb1a
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-03 16:59:16 +0100

    add blog 1 featured

--- packages/client/src/pattern/molecule/BlogMain/index.js
+++ packages/client/src/pattern/molecule/BlogMain/index.js
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react'
-import { container1 } from './BlogMain.scss'
+import { container1, container1BlogItem, container1BlogItemImage, container1BlogItemContent } from './BlogMain.scss'
 import { blogMainDefaultProps, blogMainPropTypes } from './BlogMain.props'
 import { handleStickyNavBar } from '../../../../../library/src/client/navBar'
+import { windowObjectExists } from '../../../../../library/src/environment'
+import Blog1 from '../../../asset/image/blog/blog-1.jpg'
+import { LoremIpsum } from '../../atom/placeholder/LoremIpsum'
+import { element } from '../../../../../library/src/client/dom'
+import { size } from '../../../../../library/src/client/placeholder'
 
 export function BlogMain({ className }) {
   useEffect(() => {
@@ -9,8 +14,42 @@ export function BlogMain({ className }) {
   })
 
   return (
-    <main className={className} style={{ marginTop: '55', display: 'flex', justifyContent: 'center' }}>
-      <div className={container1}>BlogMain under construction</div>
+    <main className={className} style={{ marginTop: '55px', display: 'flex', justifyContent: 'center' }}>
+      <div className={container1}>
+        <section itemScope itemType="http://schema.org/Blog">
+          <h2>
+            <a itemProp="url" href="/categories/recent">
+              <span>
+                Recent articles
+                <svg viewBox="0 0 185.343 185.343" height="16px" width="16px">
+                  <path
+                    d="M51.707 185.343a10.692 10.692 0 0 1-7.593-3.149 10.724 10.724 0 0 1 0-15.175l74.352-74.347L44.114 18.32c-4.194-4.194-4.194-10.987 0-15.175 4.194-4.194 10.987-4.194 15.18 0l81.934 81.934c4.194 4.194 4.194 10.987 0 15.175l-81.934 81.939a10.678 10.678 0 0 1-7.587 3.15z"
+                    fill="#010002"
+                  />
+                </svg>
+              </span>
+            </a>
+          </h2>
+          <article className={container1BlogItem}>
+            <div className={container1BlogItemImage}>
+              <div style={{ width: '100%', paddingBottom: '64.94140625%' }} />
+              <picture>
+                <img src={!windowObjectExists ? 'mts/static/blog-1.jpg' : Blog1} alt="blog 1" />
+              </picture>
+            </div>
+            <div className={container1BlogItemContent}>
+              <header>
+                <h2 itemProp="name headline">
+                  <a itemProp="url" aria-label="Go To Blog Post" href="/react-libraries">
+                    React Libraries in 2020
+                  </a>
+                </h2>
+              </header>
+              <LoremIpsum container={element.paragraph} length={size.small} count={1} />
+            </div>
+          </article>
+        </section>
+      </div>
     </main>
   )
 }
