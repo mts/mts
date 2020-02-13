cbf2c177c4a54a733cc66cf8a18db74c3a59bcd2
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-13 09:27:48 +0100

    add courses page path

--- packages/client/src/route/path.js
+++ packages/client/src/route/path.js
@@ -1,3 +1,4 @@
 export const homePagePath = process.env.BASE_URL ? `${process.env.BASE_URL}/` : '/'
 export const aboutPagePath = process.env.BASE_URL ? `${process.env.BASE_URL}/about` : '/about'
 export const blogPagePath = process.env.BASE_URL ? `${process.env.BASE_URL}/blog` : '/blog'
+export const coursesPagePath = process.env.BASE_URL ? `${process.env.BASE_URL}/courses` : '/courses'
