9d0b7d8c0e31d2bfeb7a2a6577f724813b5c511d
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-16 12:46:43 +0100

    rename to AboutPage.prop

--- packages/client/src/pattern/page/AboutPage/AboutPage.jsx
+++ packages/client/src/pattern/page/AboutPage/AboutPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AboutTemplate } from '../../template/AboutTemplate'
-import { aboutPageDefaultProps, aboutPagePropTypes } from './AboutPage.props'
+import { aboutPageDefaultProps, aboutPagePropTypes } from './AboutPage.prop'
 
 export function AboutPage({ className }) {
   return (
