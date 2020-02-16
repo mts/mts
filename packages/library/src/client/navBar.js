8eeb7ae63e5e7faf5f314c7d36b2821aa3ea68f5
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-16 13:22:55 +0100

    update snapshots rendering container

--- packages/library/src/client/navBar.js
+++ packages/library/src/client/navBar.js
@@ -3,7 +3,7 @@ import { navSticky, navBarLogoSticky, navBarListItemLinkSticky } from '../../../
 
 export function handleStickyNavBar() {
   const nav = document.querySelector(`[class*="Navigation_nav"]`)
-  const backgroundImage = document.querySelector(`[class*="HomeMain_container1BackgroundImage"]`)?.querySelector('picture img')
+  const backgroundImage = document.querySelector(`[class*="HomeMain_containerBackgroundImage"]`)?.querySelector('picture img')
   const navBarLogo = document.querySelector(`[class*="Navigation_navBarLogo"]`)
   const navBarMobileToggle = document.querySelector(`[class*="Navigation_navBarMobileToggle"]`)
   const navBarListItemLinks = document.querySelectorAll(`[class*="Navigation_navBarListItemLink"]`)
