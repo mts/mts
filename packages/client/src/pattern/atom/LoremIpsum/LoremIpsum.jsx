e86af81cb9b9f38206ee71a52e7e5d53939d0b3f
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-20 14:50:15 +0100

    implement lorem ipsum without container

--- packages/client/src/pattern/atom/LoremIpsum/LoremIpsum.jsx
+++ packages/client/src/pattern/atom/LoremIpsum/LoremIpsum.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import {
+  loremIpsumTextX2Small,
+  loremIpsumTextX1Small,
+  loremIpsumTextSmall,
+  loremIpsumTextMedium,
+  loremIpsumTextLarge,
+} from './LoremIpsum.part'
 import { loremIpsumDefaultProps, loremIpsumPropTypes } from './LoremIpsum.prop'
 import { element } from '../../../../../library/src/client/dom'
 import { size } from '../../../../../library/src/client/placeholder'
@@ -8,55 +15,32 @@ export function LoremIpsum({ className, container, length, count }) {
     function Paragraph() {
       switch (length) {
         case size.x2Small:
-          return (
-            <p className={className}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec turpis eleifend, pretium leo ut, euismod sapien.
-            </p>
-          )
+          return <p className={className}>{loremIpsumTextX2Small}</p>
         case size.x1Small:
-          return (
-            <p className={className}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec turpis eleifend, pretium leo ut, euismod sapien. Sed
-              quis purus quis metus sagittis sollicitudin accumsan in ante.
-            </p>
-          )
+          return <p className={className}>{loremIpsumTextX1Small}</p>
         case size.small:
-          return (
-            <p className={className}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec turpis eleifend, pretium leo ut, euismod sapien. Sed
-              quis purus quis metus sagittis sollicitudin accumsan in ante. Cras tortor nisi, vulputate in pharetra et, cursus non felis.
-              Pellentesque libero nulla, auctor ac viverra eu, gravida eu odio. Vestibulum dolor ex, mollis eget euismod et, auctor ut est.
-              Etiam posuere hendrerit tincidunt.
-            </p>
-          )
+          return <p className={className}>{loremIpsumTextSmall}</p>
         case size.medium:
-          return (
-            <p className={className}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec turpis eleifend, pretium leo ut, euismod sapien. Sed
-              quis purus quis metus sagittis sollicitudin accumsan in ante. Cras tortor nisi, vulputate in pharetra et, cursus non felis.
-              Pellentesque libero nulla, auctor ac viverra eu, gravida eu odio. Vestibulum dolor ex, mollis eget euismod et, auctor ut est.
-              Etiam posuere hendrerit tincidunt. Curabitur commodo, sapien id mattis sagittis, lectus dolor ullamcorper purus, et lacinia
-              elit urna tincidunt massa. Nam pretium nisi at vehicula placerat. Ut molestie eros in sapien mollis, tincidunt vehicula arcu
-              pellentesque. Proin vel elementum ante. Curabitur ut erat massa. Integer pellentesque mauris sed libero scelerisque molestie.
-            </p>
-          )
+          return <p className={className}>{loremIpsumTextMedium}</p>
         case size.large:
-          return (
-            <p className={className}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent nec turpis eleifend, pretium leo ut, euismod sapien. Sed
-              quis purus quis metus sagittis sollicitudin accumsan in ante. Cras tortor nisi, vulputate in pharetra et, cursus non felis.
-              Pellentesque libero nulla, auctor ac viverra eu, gravida eu odio. Vestibulum dolor ex, mollis eget euismod et, auctor ut est.
-              Etiam posuere hendrerit tincidunt. Curabitur commodo, sapien id mattis sagittis, lectus dolor ullamcorper purus, et lacinia
-              elit urna tincidunt massa. Nam pretium nisi at vehicula placerat. Ut molestie eros in sapien mollis, tincidunt vehicula arcu
-              pellentesque. Proin vel elementum ante. Curabitur ut erat massa. Integer pellentesque mauris sed libero scelerisque molestie.
-              Morbi ac dapibus urna. Donec magna felis, porta eget felis vel, sodales fringilla quam. Maecenas mauris ligula, tempus et
-              pretium quis, aliquam nec odio. Praesent mattis a leo lacinia facilisis. Integer rutrum ac purus nec blandit. Nullam et felis
-              ut eros sodales laoreet non vel ex. Suspendisse quis viverra purus. Pellentesque lobortis semper laoreet. Proin venenatis
-              lacus nec neque aliquam cursus. Nullam porttitor a nulla a venenatis. Duis eu sapien velit. Phasellus sagittis vitae ligula
-              eget posuere. Nullam volutpat cursus nunc, at molestie tellus pellentesque non. Vestibulum dignissim ante vel diam tincidunt
-              iaculis. Vestibulum vehicula lobortis ultrices.
-            </p>
-          )
+          return <p className={className}>{loremIpsumTextLarge}</p>
+        default:
+          return null
+      }
+    }
+
+    function Text() {
+      switch (length) {
+        case size.x2Small:
+          return loremIpsumTextX2Small
+        case size.x1Small:
+          return loremIpsumTextX1Small
+        case size.small:
+          return loremIpsumTextSmall
+        case size.medium:
+          return loremIpsumTextMedium
+        case size.large:
+          return loremIpsumTextLarge
         default:
           return null
       }
@@ -69,6 +53,10 @@ export function LoremIpsum({ className, container, length, count }) {
 
     if (container === element.paragraph) {
       return Array(parsedCount).fill(<Paragraph />)
+    }
+
+    if (!container) {
+      return Array(parsedCount).fill(<Text />)
     }
 
     return null
