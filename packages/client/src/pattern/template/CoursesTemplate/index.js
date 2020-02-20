db1272a83f512177cf41878dc4a7fd0e022ef261
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-20 12:23:47 +0100

    extend CoursesTemplate with CoursesContent

--- packages/client/src/pattern/template/CoursesTemplate/index.js
+++ packages/client/src/pattern/template/CoursesTemplate/index.js
@@ -4,12 +4,14 @@ import { coursesTemplate } from './CoursesTemplate.scss'
 import { coursesTemplateDefaultProps, coursesTemplatePropTypes } from './CoursesTemplate.prop'
 import { Navigation } from '../../molecule/Navigation'
 import { CoursesHeader } from '../../molecule/CoursesHeader'
+import { CoursesContent } from '../../molecule/CoursesContent'
 
 export function CoursesTemplate({ className }) {
   return (
     <div className={cx(className, coursesTemplate)}>
       <Navigation />
       <CoursesHeader />
+      <CoursesContent />
     </div>
   )
 }
