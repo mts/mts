ee00d2a758100f036ff3eb077c9244551b9c00fe
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-20 11:10:14 +0100

    extend CoursesTemplate with CoursesHeader

--- packages/client/src/pattern/template/CoursesTemplate/index.js
+++ packages/client/src/pattern/template/CoursesTemplate/index.js
@@ -3,11 +3,13 @@ import cx from 'classnames'
 import { coursesTemplate } from './CoursesTemplate.scss'
 import { coursesTemplateDefaultProps, coursesTemplatePropTypes } from './CoursesTemplate.prop'
 import { Navigation } from '../../molecule/Navigation'
+import { CoursesHeader } from '../../molecule/CoursesHeader'
 
 export function CoursesTemplate({ className }) {
   return (
     <div className={cx(className, coursesTemplate)}>
       <Navigation />
+      <CoursesHeader />
     </div>
   )
 }
