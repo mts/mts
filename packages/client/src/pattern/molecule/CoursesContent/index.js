4cd52156ad2689ffb42a9f47ec6d7aff81c952ee
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-20 14:54:33 +0100

    implement CoursesContent course roadmap

--- packages/client/src/pattern/molecule/CoursesContent/index.js
+++ packages/client/src/pattern/molecule/CoursesContent/index.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import cx from 'classnames'
-import { container } from './CoursesContent.scss'
+import {
+  content,
+  contentCourse,
+  contentCourseContainer,
+  contentCourseContainerRoadmap,
+  contentCourseContainerRoadmapDescription,
+} from './CoursesContent.scss'
+import { LoremIpsum } from '../../atom/LoremIpsum'
 import { coursesContentDefaultProps, coursesContentPropTypes } from './CoursesContent.prop'
+import { size } from '../../../../../library/src/client/placeholder'
 
 export function CoursesContent({ className }) {
-  return <div className={cx(className, container)}>CoursesContent under construction</div>
+  return (
+    <div className={cx(className, content)}>
+      <div className={contentCourse}>
+        <div className={contentCourseContainer}>
+          <div className={contentCourseContainerRoadmap}>
+            <h1>Lorem ipsum dolor sit amet</h1>
+            <h3 className={contentCourseContainerRoadmapDescription}>
+              <LoremIpsum length={size.x1Small} count={1} />
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 CoursesContent.defaultProps = coursesContentDefaultProps
