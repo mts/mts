041e7101a4590cad9230a7b66468b0a3ce111066
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2020-02-20 14:51:01 +0100

    temporarily comment out CoursesTemplate Navigation

--- packages/client/src/pattern/template/CoursesTemplate/index.js
+++ packages/client/src/pattern/template/CoursesTemplate/index.js
@@ -2,15 +2,16 @@ import React from 'react'
 import cx from 'classnames'
 import { coursesTemplate } from './CoursesTemplate.scss'
 import { coursesTemplateDefaultProps, coursesTemplatePropTypes } from './CoursesTemplate.prop'
-import { Navigation } from '../../molecule/Navigation'
+// import { Navigation } from '../../molecule/Navigation'
 import { CoursesHeader } from '../../molecule/CoursesHeader'
 import { CoursesContent } from '../../molecule/CoursesContent'
 
 export function CoursesTemplate({ className }) {
   return (
     <div className={cx(className, coursesTemplate)}>
-      <Navigation />
+      {/* <Navigation /> */}
       <CoursesHeader />
+      <div style={{ margin: '0 24px' }} />
       <CoursesContent />
     </div>
   )
