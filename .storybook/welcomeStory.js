bb8e40beef0be763720ad3f76d5c44b6ec3819e6
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-02 20:39:51 +0100

    remove former storybook setup

--- .storybook/welcomeStory.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { storiesOf } from '@storybook/react'
-import { version } from '../lerna.json'
-
-storiesOf('MTS', module).add(`v${version} 🎉`, () => <h1>Welcome to {`MTS v${version}`}</h1>)
