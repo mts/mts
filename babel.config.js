f027abd3b924ac51537f9d49e8819f0f0272cfb3
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2021-03-02 20:39:16 +0100

    update babel setup

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: [
     '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-optional-catch-binding',
     '@babel/plugin-proposal-optional-chaining',
